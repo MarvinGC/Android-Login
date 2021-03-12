438555f3e2e65a7fc890c2c4c2895488297154a4
MarvinGC <62900758+MarvinGC@users.noreply.github.com>
2021-03-11 23:51:11 -0600

    Initial commit

---
+++ app/src/main/java/com/devmarvin/login/Login.java
@@ -0,0 +1,14 @@
+package com.devmarvin.login;
+
+import android.os.Bundle;
+import android.text.NoCopySpan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
